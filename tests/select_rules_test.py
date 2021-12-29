ec22a985e955265efeb41e2be8ad361b43ee8681
Lennart Fridén <lennart@devl.se>
2021-12-29 22:57:52 +0100

    DRY select/reject tests

--- tests/select_rules_test.py
+++ tests/select_rules_test.py
@@ -4,23 +4,20 @@ from rules_engine import Rule, RulesEngine, then, when
 RULE_1 = Rule(when("A"), then(1), tags=["vowel"])
 RULE_2 = Rule(when("B"), then(2), tags=["even", "consonant"])
 RULE_3 = Rule(when("C"), then(3), tags=["consonant"])
+ENGINE = RulesEngine(RULE_1, RULE_2, RULE_3)
 
 
 def test_reject_rules_with_a_certain_tag():
-    engine = RulesEngine(RULE_1, RULE_2, RULE_3).reject("even")
-    assert engine.rules == (RULE_1, RULE_3)
+    assert ENGINE.reject("even").rules == (RULE_1, RULE_3)
 
 
 def test_reject_rules_using_multiple_tags():
-    engine = RulesEngine(RULE_1, RULE_2, RULE_3).reject("even", "vowel")
-    assert engine.rules == (RULE_3,)
+    assert ENGINE.reject("even", "vowel").rules == (RULE_3,)
 
 
 def test_select_rules_with_a_certain_tag():
-    engine = RulesEngine(RULE_1, RULE_2, RULE_3).select("consonant")
-    assert engine.rules == (RULE_2, RULE_3)
+    assert ENGINE.select("consonant").rules == (RULE_2, RULE_3)
 
 
 def test_select_rules_using_multiple_tags():
-    engine = RulesEngine(RULE_1, RULE_2, RULE_3).select("even", "vowel")
-    assert engine.rules == (RULE_1, RULE_2)
+    assert ENGINE.select("even", "vowel").rules == (RULE_1, RULE_2)
